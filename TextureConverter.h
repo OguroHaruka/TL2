dbe406c5a66b25a339a8939cdfb24a70263ce1c7
Oguro Haruka <Oguro Haruka@MSI>
2024-11-26 05:17:59 +0900

    a

---
+++ TextureConverter.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<string>
+
+class TextureConverter
+{
+public:
+	TextureConverter();
+	~TextureConverter();
+
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConverteTextureWICToDDS(const std::string& filePath);
+private: 
+
+	/// <summary>
+	/// テクスチャファイルの読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	/// <summary>
+	/// マルチバイト文字をワイド文字に変換する
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns></returns>
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+};
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include "TextureConverter.h"
+#include "Windows.h"
+
+enum Argument {
+	kApplicationPath,	//アプリケーションのパス
+	kFilePath,			//渡されたファイルのパス
+
+	NumArugument,
+	__END__
+};
 
 int main(int argc, char* argv[]) {
-	for (int i = 0; i < argc; i++) {
-		printf(argv[i]);
-		printf("\n");
-	}
+	assert(argc >= NumArugument);
+
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	TextureConverter textureConverter;
+	textureConverter.ConverteTextureWICToDDS(argv[kFilePath]);
+
+	CoUninitialize();
 	system("pause");
 	return 0;
 }
