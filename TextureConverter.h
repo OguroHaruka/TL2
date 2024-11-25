95d809a1e3374dc90f3987db36a7413d95e1c791
Oguro Haruka <Oguro Haruka@MSI>
2024-11-26 06:44:43 +0900

    完了

--- TextureConverter.h
+++ TextureConverter.h
@@ -28,8 +28,25 @@ private:
 	/// <returns></returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
 
-	DirectX::TexMetadata metadata_;
+	/// <summary>
+	/// フォルダパスとファイル名を分解する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
 
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+	DirectX::TexMetadata metadata_;
 	DirectX::ScratchImage scratchImage_;
+	
+	// ディレクトリパス
+	std::wstring directoryPath_;
+	// ファイル名
+	std::wstring fileName_;
+	// ファイル拡張子
+	std::wstring fileExt_;
 };
 
